b4cbd7a5364b70cd91234169ba1f428b517655be
kosmo <kosmosensei@gmail.com>
2023-06-25 20:07:15 +0700

    Initial commit

---
+++ src/main/java/sky/pro/lesson/hwbin/HwBinApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.lesson.hwbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwBinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/lesson/hwbin/HwBinApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.lesson.hwbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
